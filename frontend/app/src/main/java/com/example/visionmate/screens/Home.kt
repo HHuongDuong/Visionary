a76928a68a4193230537406c0e7379dc683a444d
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-10-09 17:11:33 +0700

    camera

--- frontend/app/src/main/java/com/example/visionmate/screens/Home.kt
+++ frontend/app/src/main/java/com/example/visionmate/screens/Home.kt
@@ -5,6 +5,10 @@ import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
@@ -12,11 +16,39 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import androidx.compose.material3.Text
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.example.visionmate.ui.components.CameraPreview
 
 @Composable
 fun Home() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        // Add your UI here
-        Text("Home Screen")
+    val context = LocalContext.current
+    var hasCameraPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        hasCameraPermission = isGranted
+        if (!isGranted) {
+            Toast.makeText(context, "Camera permission denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    LaunchedEffect(key1 = hasCameraPermission) {
+        if (!hasCameraPermission) {
+            launcher.launch(Manifest.permission.CAMERA)
+        }
+    }
+
+    if (hasCameraPermission) {
+        CameraPreview()
+    } else {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text("Camera permission is required to use this feature.")
+        }
     }
 }
