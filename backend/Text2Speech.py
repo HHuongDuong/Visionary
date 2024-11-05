df29946806d84b51a6942b1a986a982c4754ff06
Quannggg <nhieuvai@gmail.com>
2024-11-05 20:35:09 +0700

    fix status code and add new voice

---
+++ backend/Text2Speech.py
@@ -0,0 +1,31 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def text_to_speech( text, language='vi-vn', format='mp3', speed='0', pitch='0'):
+    url = 'https://api.voicerss.org/'
+    params = {
+        'key': os.getenv("Voice_RSS") ,  # API key,
+        'hl': language,  # Ngôn ngữ: vi-vn (Tiếng Việt), en-us (Tiếng Anh), v.v.
+        'src': text,     # Văn bản cần chuyển đổi
+        'r': speed,      # Tốc độ: -10 đến 10
+        'p': pitch,      # Độ cao: -10 đến 10
+        'c': format      # Định dạng: mp3, wav, v.v.
+    }
+
+    response = requests.get(url, params=params)
+
+
+    if response.status_code == 200:
+        filename = 'output.mp3'
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+            return filename
+        print('Audio file saved as output.mp3')
+    else:
+        print(f'Error: {response.status_code} - {response.text}')
+
+text = '500000 VNĐ'
+text_to_speech( text)
