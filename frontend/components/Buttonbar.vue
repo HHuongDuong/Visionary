23133b9a72610e342a863a1c64dc5c14a4056ef4
WindBow27 <phonglunhi27@gmail.com>
2024-11-06 16:44:28 +0700

    recolor

--- frontend/components/Buttonbar.vue
+++ frontend/components/Buttonbar.vue
@@ -2,7 +2,7 @@
     <div class="fixed bottom-0 left-0 w-full overflow-x-auto no-scrollbar p-8">
         <div class="flex space-x-8 after:content-[''] after:flex-shrink-0 after:w-8">
             <button v-for="(button, index) in buttons" :key="index"
-                :class="{ 'btn btn-outline btn-primary h-20 w-20 flex flex-col items-center justify-center': 
+                :class="{ 'btn btn-secondary h-20 w-20 flex flex-col items-center justify-center': 
                 selectedButton !== index, 'btn btn-primary h-20 w-20 flex flex-col items-center justify-center': selectedButton === index }"
                 @click="selectButton(index)">
                 <i :class="icons[index]"></i>
